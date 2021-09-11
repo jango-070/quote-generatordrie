06de11b4db4f1974c42f9cf31b3f05a4e195285c
Jango-070 <jango.room@hotmail.com>
2021-09-11 16:33:22 +0200

    toegevoegd documenten

---
+++ quotes.js
@@ -0,0 +1,5 @@
+const localQuotes = []
+{
+  text: 'testting', 
+  author: 'Jango',   
+},
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('testing');
+let apiQuotes = []; 
+
+// Show new quote function
+function newQuote() {
+    // pick a random quote from api quotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+
+// Get quotes from API
+async function getQuotes () {
+    const apiUrl = 'https://type.fit/api/quotes'; 
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch (error) {
+        //  Cach Error Here
+    }
+}
+
+// On Load
+getQuotes();
